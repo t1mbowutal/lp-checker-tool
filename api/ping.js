04cd34959dd2379722e16a124c39515cb100c5df
t1mbowutal <tim.n.clausen@gmail.com>
2025-09-23 22:42:15 +0200

    Delete api directory

--- api/ping.js
+++
@@ -1,7 +0,0 @@
-export const config = { runtime: 'edge' };
-
-export default async function handler(req) {
-  return new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
-    headers: { 'content-type': 'application/json', 'access-control-allow-origin': '*' }
-  });
-}
