69533ead7a326ee6e5b899e3db7e108d219b7bd9
t1mbowutal <tim.n.clausen@gmail.com>
2025-09-24 13:59:38 +0200

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body className="app">{children}</body>
     </html>
   );
 }
